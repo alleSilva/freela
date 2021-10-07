626bc36f5ec0e43dd87cf2a5e2d70971a55bd23c
Alesandro Silva <silva_alesandro@outlook.com>
2021-10-07 10:23:49 -0400

    create navbar with Entrar e Registre-se links

--- spec/system/visitor_visit_homepage_spec.rb
+++ spec/system/visitor_visit_homepage_spec.rb
@@ -6,5 +6,8 @@ describe 'Visitor visit homepage' do
 
     expect(page).to have_css('h1', text: 'Freela')
     expect(page).to have_css('h3', text: 'Boas vindas ao sistema de contratação de frelancers')
+    expect(page).to have_link('Cadastre-se')
+    expect(page).to have_link('Entrar')
+
   end
 end
