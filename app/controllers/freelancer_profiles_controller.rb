1c69e3399538e4738f93d0d57e3792fdfa90fae6
Alesandro Silva <silva_alesandro@outlook.com>
2021-10-16 00:43:24 -0400

    add create action with strong params to freelancer_profile controller

--- app/controllers/freelancer_profiles_controller.rb
+++ app/controllers/freelancer_profiles_controller.rb
@@ -4,5 +4,13 @@ class FreelancerProfilesController < ApplicationController
   end
 
   def create
+    @freelancer_profile = FreelancerProfile.create!(params.require(:freelancer_profile).permit(
+      :complete_name,
+      :social_name,
+      :birth_date,
+      :education,
+      :description,
+      :experience,
+    ))
   end
 end
