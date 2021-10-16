df40200c5b641e2fc3db05dcb3b8595836d18eb0
Alesandro Silva <silva_alesandro@outlook.com>
2021-10-16 12:02:32 -0400

    add redirecion to freelancer profile after signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :freelancers
+  devise_for :freelancers, controllers: { registrations: "registrations" }
   devise_for :project_owners
 
   root to: 'home#index'
