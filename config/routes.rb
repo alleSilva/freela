b6e06485c5b916396fbb34ca7fb465a9adcc6e97
Alesandro Silva <silva_alesandro@outlook.com>
2021-10-17 23:05:08 -0400

    frellancers can view projects

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/sign_in', to: 'sign_in#index', as: 'sign_in_page'
   get '/sign_up', to: 'sign_up#index', as: 'sign_up_page'
   
-  resources :projects, only: [:show, :new, :create] do
+  resources :projects, only: [:index, :show, :new, :create] do
     get 'my_projects', on: :collection
   end
   resources :freelancers, only: [:show, :new, :create]
