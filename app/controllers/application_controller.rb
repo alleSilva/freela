1ec9ddd0a5c07c29c79aefe8499a18052c4b4442
Alesandro Silva <silva_alesandro@outlook.com>
2021-10-29 02:26:11 -0400

    redirect a freelancer to profile form if it is empty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if freelancer_signed_in? && current_freelancer.freelancer_profile.blank?
+      new_freelancer_profile_path(current_freelancer.id)
+    else
+      root_path
+    end
+  end
 end
