ebf5e0287e6d4d6197b4ee07058fabc0095645c8
Alesandro <silva_alesandro@outlook.com>
2021-10-31 23:32:16 -0400

    fix project_spec test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Project, type: :model do
           skills: "Ruby, SQL, Json",
           max_payment_hour: 100,
           remote: true,
-          limit_bid_date: I18n.l(Date.yesterday),
+          limit_bid_date: 7.day.ago,
           project_owner: owner
         })
 
@@ -21,5 +21,42 @@ RSpec.describe Project, type: :model do
         expect(project.errors[:limit_bid_date]).to include("não pode está em datas passadas")
       end
     end
+
+    context 'should be valid' do
+      it 'limit bid date is in the future' do
+        owner = ProjectOwner.create!(email: 'owner@email.com.br', password: '123456')
+        
+        project = Project.new({
+          title: "Api de entregas",
+          description: "Api para servir de backend para aplicativo",
+          skills: "Ruby, SQL, Json",
+          max_payment_hour: 100,
+          remote: true,
+          limit_bid_date: 7.days.from_now,
+          project_owner: owner
+        })
+
+        expect(project).to be_valid
+      end
+    end
+
+    context 'should be valid' do
+      it 'limit bid date is today' do
+        owner = ProjectOwner.create!(email: 'owner@email.com.br', password: '123456')
+        
+        project = Project.new({
+          title: "Api de entregas",
+          description: "Api para servir de backend para aplicativo",
+          skills: "Ruby, SQL, Json",
+          max_payment_hour: 100,
+          remote: true,
+          limit_bid_date: Date.today,
+          project_owner: owner
+        })
+
+        expect(project).to be_valid
+      end
+    end
+
   end
 end
